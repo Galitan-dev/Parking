782eaf17510fbfa2a5e0771bc5849bbfc6191ef2
Galitan-dev <galitan.dev@gmail.com>
2023-04-23 11:42:03 +0200

    fix: remove sample test

--- spec/parking_spec.rb
+++ spec/parking_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Parking do
   it "has a version number" do
     expect(Parking::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
